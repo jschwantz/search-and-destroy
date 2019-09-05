ec029338c1bc494f46d9b8067db27c4d249c6cbf
Jonathan Galletta <jonathan.galletta@gmail.com>
2019-09-05 17:32:04 -0400

    finished workshop

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,17 @@
 'use strict';
 
 // Complete this algo
-const minJumps = arr => {
 
+const minJumps = arr => {
+  let counter = 1;
+  let latestIndex = 0;
+  let len = arr.length - 1;
+  console.log(arr);
+  if (arr[0] + latestIndex >= len) return counter;
+  for (let i = arr.length - 2; i >= 0; i--) {
+    if (arr[i] + i >= len) latestIndex = i;
+  }
+  return counter + minJumps(arr.slice(0, latestIndex + 1));
 };
 
-module.exports = minJumps
+module.exports = minJumps;
